d28c20147ced5c56b7ccdd72b974af9454d0fbe6
ryota.okamoto <r.okamoto@ridecore.co.jp>
2023-10-12 18:32:10 +0900

    routing mailaddress

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # ==================================================
+  # メールアドレス
+  # ==================================================
+  resources :mailaddress, only: [:index, :new, :create]
 end
